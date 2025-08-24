cf7fedd1e1d08efff3f4f369b66dd9cb52a544fe
aki1104 <steevenparubrub@gmail.com>
2025-08-24 18:49:30 +0800

    logout confirmation backend for shopowner

--- shopowner/js/shopowner_editshoe.js
+++ shopowner/js/shopowner_editshoe.js
@@ -413,3 +413,14 @@ window.removeSizeInput = removeSizeInput;
 window.removeVariant = removeVariant;
 window.addColorVariantWithData = addColorVariantWithData;
 window.resetStock = resetStock;
+
+// Logout functionality
+document.getElementById('logout_btn').addEventListener('click', function() {
+    if (confirm('Are you sure you want to logout?')) {
+        auth.signOut().then(() => {
+            window.location.href = '/user_login.html';
+        }).catch((error) => {
+            console.error('Error signing out:', error);
+        });
+    }
+});
